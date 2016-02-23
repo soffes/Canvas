87043a8f1da4bdf4d01e5315126f4a5894ed3914
Sam Soffes <sam@soff.es>
2016-02-23 12:43:57 -0800

    Canvas Labs, Inc.

--- OperationTransport/OperationTransport.h
+++ OperationTransport/OperationTransport.h
@@ -3,7 +3,7 @@
 //  OperationTransport
 //
 //  Created by Sam Soffes on 2/17/16.
-//  Copyright © 2016 Canvas Labs Inc. All rights reserved.
+//  Copyright © 2016 Canvas Labs, Inc. All rights reserved.
 //
 
 @import Foundation;
--- OperationTransportTests/OperationTransportTests.swift
+++ OperationTransportTests/OperationTransportTests.swift
@@ -3,7 +3,7 @@
 //  OperationTransportTests
 //
 //  Created by Sam Soffes on 2/17/16.
-//  Copyright © 2016 Canvas Labs Inc. All rights reserved.
+//  Copyright © 2016 Canvas Labs, Inc. All rights reserved.
 //
 
 import XCTest
