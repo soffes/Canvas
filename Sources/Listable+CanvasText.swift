2ff13348f04408997686bef6bad26ebebb34a2be
Sam Soffes <sam@soff.es>
2016-04-19 13:18:26 -0700

    Move isFilled to CanvasText

---
+++ Sources/Listable+CanvasText.swift
@@ -0,0 +1,15 @@
+//
+//  Listable+CanvasText.swift
+//  CanvasText
+//
+//  Created by Sam Soffes on 4/19/16.
+//  Copyright © 2016 Canvas Labs, Inc. All rights reserved.
+//
+
+import CanvasNative
+
+extension Indentation {
+	var isFilled: Bool {
+		return rawValue % 2 == 0
+	}
+}
