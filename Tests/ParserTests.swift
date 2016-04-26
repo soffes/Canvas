1652b6edde6b1dd375a47a3a48f1e2cdffd23a1b
Sam Soffes <sam@soff.es>
2016-04-26 13:13:41 -0700

    Refactor ParserTests

---
+++ Tests/ParserTests.swift
@@ -0,0 +1,24 @@
+//
+//  ParserTests.swift
+//  CanvasNative
+//
+//  Created by Sam Soffes on 4/26/16.
+//  Copyright © 2016 Canvas Labs, Inc. All rights reserved.
+//
+
+import XCTest
+import CanvasNative
+
+// Most of the parser is tested in the node tests.
+class ParserTests: XCTestCase {
+	func testTrailingNewLine() {
+		let blocks = Parser.parse("⧙doc-heading⧘Hello\n")
+
+		XCTAssert(blocks[0] is Title)
+		XCTAssertEqual(NSRange(location: 0, length: 18), blocks[0].range)
+
+		XCTAssertEqual(2, blocks.count)
+//		XCTAssert(blocks[1] is Paragraph)
+//		XCTAssertEqual(NSRange(location: 19, length: 0), blocks[1].range)
+	}
+}
--- Tests/TitleTests.swift
+++ Tests/TitleTests.swift
@@ -22,15 +22,4 @@ class TitleTest: XCTestCase {
 		XCTAssert(node.subnodes[0] is Text)
 		XCTAssert(node.subnodes[1] is DoubleEmphasis)
 	}
-
-	func testTrailingNewLine() {
-		let blocks = Parser.parse("⧙doc-heading⧘Hello\n")
-
-		XCTAssert(blocks[0] is Title)
-		XCTAssertEqual(NSRange(location: 0, length: 18), blocks[0].range)
-
-		XCTAssertEqual(2, blocks.count)
-//		XCTAssert(blocks[1] is Paragraph)
-//		XCTAssertEqual(NSRange(location: 19, length: 0), blocks[1].range)
-	}
 }
