f1e6f9b79ef9d87dade8f754baa2fecd440a5d10
Sam Soffes <sam@soff.es>
2016-05-10 10:57:10 -0700

    Add CanvasNative.nativeVersion

---
+++ Sources/CanvasNative.swift
@@ -0,0 +1,12 @@
+//
+//  CanvasNative.swift
+//  CanvasNative
+//
+//  Created by Sam Soffes on 5/10/16.
+//  Copyright © 2016 Canvas Labs, Inc. All rights reserved.
+//
+
+public struct CanvasNative {
+	/// The version of Canvas Native this library can parse.
+	public static let nativeVersion = "0.0.1"
+}
