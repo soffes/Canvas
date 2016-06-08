97e3d32d7fd637669bc39bc2bfde78188ad701d2
Sam Soffes <sam@soff.es>
2016-06-07 17:45:05 -0700

    Move more things into shared theme

--- Sources/Theme.swift
+++ Sources/Theme.swift
@@ -38,9 +38,13 @@ public protocol Theme {
 	var codeBlockLineNumberBackgroundColor: Color { get }
 	var blockquoteColor: Color { get }
 	var blockquoteBorderColor: Color { get }
+
+	var foldedColor: Color { get }
 	var strikethroughColor: Color { get }
 	var commentBackgroundColor: Color { get }
 	var linkURLColor: Color { get }
+	var codeSpanColor: Color { get }
+	var codeSpanBackgroundColor: Color { get }
 
 	var placeholderImageColor: Color { get }
 	var placeholderImageBackgroundColor: Color { get }
@@ -62,6 +66,14 @@ public protocol Theme {
 
 
 extension Theme {
+	public var fontSize: CGFloat {
+		return 18
+	}
+	
+	private var listIndentation: CGFloat {
+		return round(fontSize * 1.1)
+	}
+
 	public var baseAttributes: Attributes {
 		return [
 			NSForegroundColorAttributeName: foregroundColor,
@@ -73,6 +85,13 @@ extension Theme {
 		var attributes = baseAttributes
 		attributes[NSForegroundColorAttributeName] = foregroundColor
 		attributes[NSFontAttributeName] = fontOfSize(round(fontSize * 1.7), symbolicTraits: [.TraitBold])
+		return attributes
+	}
+
+	public func foldingAttributes(currentFont currentFont: X.Font) -> Attributes {
+		var attributes = baseAttributes
+		attributes[NSForegroundColorAttributeName] = foldedColor
+		attributes[NSFontAttributeName] = currentFont
 		return attributes
 	}
 
@@ -115,5 +134,164 @@ extension Theme {
 		#endif
 
 		return font
+	}
+
+	public func blockSpacing(block block: BlockNode, horizontalSizeClass: UserInterfaceSizeClass) -> BlockSpacing {
+		var spacing = BlockSpacing(marginBottom: round(fontSize * 1.5))
+
+		// No margin if it's not at the bottom of a positionable list
+		if let block = block as? Positionable where !(block is Blockquote) {
+			if !block.position.isBottom {
+				spacing.marginBottom = 4
+			}
+		}
+
+		// Heading spacing
+		if block is Heading {
+			spacing.marginTop = round(spacing.marginBottom * 0.25)
+			spacing.marginBottom = round(spacing.marginBottom / 2)
+			return spacing
+		}
+
+		// Indentation
+		if let listable = block as? Listable {
+			spacing.paddingLeft = round(listIndentation * CGFloat(listable.indentation.rawValue + 1))
+			return spacing
+		}
+
+		if let code = block as? CodeBlock {
+			let padding: CGFloat = 16
+			let margin: CGFloat = 5
+
+			// Top margin
+			if code.position.isTop {
+				spacing.paddingTop += padding
+				spacing.marginTop += margin
+			}
+
+			// Bottom margin
+			if code.position.isBottom {
+				spacing.paddingBottom += padding
+				spacing.marginBottom += margin
+			}
+
+			spacing.paddingLeft = listIndentation
+
+			// Indent for line numbers
+			if horizontalSizeClass == .Regular {
+				// TODO: Don't hard code
+				spacing.paddingLeft += 40
+			}
+
+			return spacing
+		}
+
+		if let blockquote = block as? Blockquote {
+			let padding: CGFloat = 4
+
+			// Top margin
+			if blockquote.position.isTop {
+				spacing.paddingTop += padding
+			}
+
+			// Bottom margin
+			if blockquote.position.isBottom {
+				spacing.paddingBottom += padding
+			}
+
+			spacing.paddingLeft = listIndentation
+
+			return spacing
+		}
+
+		return spacing
+	}
+
+	public func attributes(block block: BlockNode) -> Attributes {
+		if block is Title {
+			return titleAttributes
+		}
+
+		var attributes = baseAttributes
+
+		if let heading = block as? Heading {
+			switch heading.level {
+			case .One:
+				attributes[NSForegroundColorAttributeName] = foregroundColor
+				attributes[NSFontAttributeName] = fontOfSize(round(fontSize * 1.5), symbolicTraits: .TraitBold)
+			case .Two:
+				attributes[NSForegroundColorAttributeName] = foregroundColor
+				attributes[NSFontAttributeName] = fontOfSize(round(fontSize * 1.2), symbolicTraits: .TraitBold)
+			case .Three:
+				attributes[NSForegroundColorAttributeName] = foregroundColor
+				attributes[NSFontAttributeName] = fontOfSize(round(fontSize * 1.1), symbolicTraits: .TraitBold)
+			case .Four:
+				attributes[NSForegroundColorAttributeName] = foregroundColor
+				attributes[NSFontAttributeName] = fontOfSize(fontSize, symbolicTraits: .TraitBold)
+			case .Five:
+				attributes[NSForegroundColorAttributeName] = foregroundColor
+			case .Six:
+				attributes[NSForegroundColorAttributeName] = foregroundColor
+			}
+		}
+
+		else if block is CodeBlock {
+			attributes[NSForegroundColorAttributeName] = codeColor
+			attributes[NSFontAttributeName] = monospaceFontOfSize(fontSize)
+
+			// Indent wrapped lines in code blocks
+			let paragraph = NSMutableParagraphStyle()
+			paragraph.headIndent = floor(fontSize * 1.2) + 0.5
+			attributes[NSParagraphStyleAttributeName] = paragraph
+		}
+
+		else if block is Blockquote {
+			attributes[NSForegroundColorAttributeName] = blockquoteColor
+		}
+
+		return attributes
+	}
+
+	public func attributes(span span: SpanNode, currentFont: X.Font) -> Attributes? {
+		var traits = currentFont.symbolicTraits
+		var attributes = Attributes()
+
+		if span is CodeSpan {
+			attributes[NSFontAttributeName] = monospaceFontOfSize(fontSize, symbolicTraits: traits)
+			attributes[NSForegroundColorAttributeName] = codeSpanColor
+			attributes[NSBackgroundColorAttributeName] = codeSpanBackgroundColor
+		}
+
+		else if span is Strikethrough {
+			attributes[NSStrikethroughStyleAttributeName] = NSUnderlineStyle.StyleThick.rawValue
+			attributes[NSStrikethroughColorAttributeName] = strikethroughColor
+			attributes[NSForegroundColorAttributeName] = strikethroughColor
+		}
+
+		else if span is DoubleEmphasis {
+			traits.insert(.TraitBold)
+			attributes[NSFontAttributeName] = fontOfSize(fontSize, symbolicTraits: traits)
+		}
+
+		else if span is Emphasis {
+			traits.insert(.TraitItalic)
+			attributes[NSFontAttributeName] = fontOfSize(fontSize, symbolicTraits: traits)
+		}
+
+		else if span is Link {
+			attributes[NSForegroundColorAttributeName] = tintColor
+		}
+
+		// If there aren't any attributes set yet, return nil and inherit from parent.
+		if attributes.isEmpty {
+			return nil
+		}
+
+		// Ensure a font is set
+		if attributes[NSFontAttributeName] == nil {
+			attributes[NSFontAttributeName] = currentFont
+		}
+		
+		return attributes
 	}
 }
