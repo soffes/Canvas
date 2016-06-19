fb9575391a8f822bada7d384733ba8913481b8ba
Sam Soffes <sam@soff.es>
2016-06-19 11:19:09 -0700

    Initial Commit

---
+++ CanvasCore/CanvasCore.h
@@ -0,0 +1,19 @@
+//
+//  CanvasCore.h
+//  CanvasCore
+//
+//  Created by Sam Soffes on 6/19/16.
+//  Copyright © 2016 Canvas Labs, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CanvasCore.
+FOUNDATION_EXPORT double CanvasCoreVersionNumber;
+
+//! Project version string for CanvasCore.
+FOUNDATION_EXPORT const unsigned char CanvasCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CanvasCore/PublicHeader.h>
+
+
