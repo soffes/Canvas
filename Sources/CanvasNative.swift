6a4f60928fdd28bb9a6900e6d8a088951f6b6a43
Sam Soffes <sam@soff.es>
2016-06-22 14:05:40 -0700

    Add version 0.1.0 to supported versions

--- Sources/CanvasNative.swift
+++ Sources/CanvasNative.swift
@@ -7,7 +7,7 @@
 //
 
 /// The version of Canvas Native this library can parse.
-let supportedNativeVersion = Set<String>(["0.0.0", "0.0.1"])
+let supportedNativeVersion = Set<String>(["0.0.0", "0.0.1", "0.1.0"])
 
 /// Is a given Canvas Native version supported?
 public func supports(nativeVersion nativeVersion: String) -> Bool {
