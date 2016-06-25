57afb1c1e8909a482ecaf30cdbfd3fc38fa34cfd
Sam Soffes <sam@soff.es>
2016-06-25 09:02:09 -0700

    Rename Color to Swatch

--- Sources/LightTheme.swift
+++ Sources/LightTheme.swift
@@ -13,42 +13,42 @@ public struct LightTheme: Theme {
 
 	// MARK: - Primary Colors
 
-	public let backgroundColor = Color.white
-	public let foregroundColor = Color.black
+	public let backgroundColor = Swatch.white
+	public let foregroundColor = Swatch.black
 	public var tintColor: X.Color
 
 
 	// MARK: - Block Colors
 	
-	public let titlePlaceholderColor = Color.lightGray
-	public let bulletColor = Color.gray
-	public let uncheckedCheckboxColor = Color.gray
-	public let orderedListItemNumberColor = Color.gray
-	public let codeColor = Color.gray
-	public let codeBlockBackgroundColor = Color.extraLightGray
-	public let codeBlockLineNumberColor = Color.lightGray
-	public let codeBlockLineNumberBackgroundColor = Color.lightGray
-	public let blockquoteColor = Color.gray
-	public let blockquoteBorderColor = Color.lightGray
-	public let headingOneColor = Color.black
-	public let headingTwoColor = Color.black
-	public let headingThreeColor = Color.black
-	public let headingFourColor = Color.black
-	public let headingFiveColor = Color.black
-	public let headingSixColor = Color.black
-	public let horizontalRuleColor = Color.gray
-	public let imagePlaceholderColor = Color.gray
-	public let imagePlaceholderBackgroundColor = Color.extraLightGray
+	public let titlePlaceholderColor = Swatch.lightGray
+	public let bulletColor = Swatch.gray
+	public let uncheckedCheckboxColor = Swatch.gray
+	public let orderedListItemNumberColor = Swatch.gray
+	public let codeColor = Swatch.gray
+	public let codeBlockBackgroundColor = Swatch.extraLightGray
+	public let codeBlockLineNumberColor = Swatch.lightGray
+	public let codeBlockLineNumberBackgroundColor = Swatch.lightGray
+	public let blockquoteColor = Swatch.gray
+	public let blockquoteBorderColor = Swatch.lightGray
+	public let headingOneColor = Swatch.black
+	public let headingTwoColor = Swatch.black
+	public let headingThreeColor = Swatch.black
+	public let headingFourColor = Swatch.black
+	public let headingFiveColor = Swatch.black
+	public let headingSixColor = Swatch.black
+	public let horizontalRuleColor = Swatch.gray
+	public let imagePlaceholderColor = Swatch.gray
+	public let imagePlaceholderBackgroundColor = Swatch.extraLightGray
 
 
 	// MARK: - Span Colors
 
-	public let foldedColor = Color.gray
-	public let strikethroughColor = Color.gray
-	public let linkURLColor = Color.gray
-	public let codeSpanColor = Color.gray
-	public let codeSpanBackgroundColor = Color.extraLightGray
-	public let commentBackgroundColor = Color.comment
+	public let foldedColor = Swatch.gray
+	public let strikethroughColor = Swatch.gray
+	public let linkURLColor = Swatch.gray
+	public let codeSpanColor = Swatch.gray
+	public let codeSpanBackgroundColor = Swatch.extraLightGray
+	public let commentBackgroundColor = Swatch.comment
 
 
 	// MARK: - Initializers
---
+++ Sources/Swatch.swift
@@ -0,0 +1,46 @@
+//
+//  Swatch.swift
+//  CanvasCore
+//
+//  Created by Sam Soffes on 11/12/15.
+//  Copyright © 2015–2016 Canvas Labs, Inc. All rights reserved.
+//
+
+import X
+
+public struct Swatch {
+
+	// MARK: - Base
+
+	public static let black = Color(red: 0.161, green: 0.180, blue: 0.192, alpha: 1)
+	public static let white = Color.whiteColor()
+	public static let gray = Color(red: 0.514, green: 0.569, blue: 0.592, alpha: 1)
+	public static let lightGray = Color(red: 0.906, green: 0.918, blue: 0.925, alpha: 1)
+	public static let extraLightGray = Color(red: 0.961, green: 0.969, blue: 0.976, alpha: 1)
+
+	private static let blue = Color(red: 0.255, green:0.306, blue: 0.976, alpha: 1)
+	private static let green = Color(red: 0.157, green:0.859, blue: 0.404, alpha: 1)
+	private static let pink = Color(red: 1, green: 0.216, blue: 0.502, alpha: 1)
+	private static let yellow = Color(red: 1, green: 0.942, blue: 0.716, alpha: 1)
+
+	// MARK: - Shared
+
+	public static let brand = blue
+	public static let destructive = pink
+	public static let comment = yellow
+
+
+	// MARK: - Bars
+
+	public static let navigationBarBorder = lightGray
+	public static let searchBarBorder = lightGray
+
+
+	// MARK: - Tables
+
+	public static let groupedTableBackground = extraLightGray
+	public static let cellSeparator = lightGray
+
+	/// Chevron in table view cells
+	public static let cellDisclosureIndicator = gray
+}
