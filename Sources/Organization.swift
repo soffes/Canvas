01e3a13f499a2a90fd6fb8c401331d3bb24387f2
Sam Soffes <sam@soff.es>
2016-07-19 20:08:50 -0600

    Rename Color to Organization.Color

--- Sources/Organization.swift
+++ Sources/Organization.swift
@@ -8,6 +8,57 @@
 
 public struct Organization {
 
+	// MARK: - Types
+
+	public struct Color {
+		public var red: Double
+		public var green: Double
+		public var blue: Double
+
+		public var hex: String {
+			return String(Int(red * 255), radix: 16) + String(Int(green * 255), radix: 16) + String(Int(blue * 255), radix: 16)
+		}
+
+		// From https://github.com/soffes/X
+		public init?(hex string: String) {
+			var hex = string as NSString
+
+			// Remove `#` and `0x`
+			if hex.hasPrefix("#") {
+				hex = hex.substringFromIndex(1)
+			} else if hex.hasPrefix("0x") {
+				hex = hex.substringFromIndex(2)
+			}
+
+			// Invalid if not 3, 6, or 8 characters
+			let length = hex.length
+			if length != 3 && length != 6 && length != 8 {
+				return nil
+			}
+
+			// Make the string 8 characters long for easier parsing
+			if length == 3 {
+				let r = hex.substringWithRange(NSRange(location: 0, length: 1))
+				let g = hex.substringWithRange(NSRange(location: 1, length: 1))
+				let b = hex.substringWithRange(NSRange(location: 2, length: 1))
+				hex = r + r + g + g + b + b + "ff"
+			} else if length == 6 {
+				hex = String(hex) + "ff"
+			}
+
+			// Convert 2 character strings to CGFloats
+			func hexValue(string: String) -> Double {
+				let value = Double(strtoul(string, nil, 16))
+				return value / 255
+			}
+
+			red = hexValue(hex.substringWithRange(NSRange(location: 0, length: 2)))
+			green = hexValue(hex.substringWithRange(NSRange(location: 2, length: 2)))
+			blue = hexValue(hex.substringWithRange(NSRange(location: 4, length: 2)))
+		}
+	}
+
+
 	// MARK: - Properties
 
 	public let id: String
